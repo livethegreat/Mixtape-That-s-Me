5150dd1d76d51eaf6190aaabd9510f212b663c33
livethegreat <130147348+livethegreat@users.noreply.github.com>
2023-08-16 04:39:56 -0700

    Add files via upload

---
+++ lyrics.js
@@ -0,0 +1,664 @@
+
+
+
+let test = document.getElementById('test')
+let test2 = document.getElementById('test2')
+let test3 = document.getElementById('test3')
+let test4 = document.getElementById('test4')
+let test5 = document.getElementById('test5')
+let test6 = document.getElementById('test6')
+
+let call = document.getElementById('call')
+
+
+
+
+function FAQ() {
+
+
+        test.innerHTML = `<div id="test"><button id="accordion" onclick="closing()">
+        
+        <div id="big"><p id="call">Win</p></div>
+        
+        <div id="small"><ion-icon name="remove-outline"></ion-icon></div>
+        
+        <div id="move-el" data-aos="fade-down" data-aos-duration="700" ><p id="quote">"If I die today my name dies today, now i'm working my ass off to make it live"</p>
+
+        <a href="win.html"><div id="view">VIEW</div></a></div>
+        
+        </button></div>
+        
+        
+       
+        
+        
+        `
+         
+            
+       
+    
+}
+
+
+function FAQ2() {
+
+
+    test2.innerHTML = `<div id="test2"><button id="accordion" onclick="closing2()">
+    
+    <div id="big"><p id="call">At This Point</p></div>
+    
+    <div id="small"><ion-icon name="remove-outline"></ion-icon></div>
+    
+    <div id="move-el" data-aos="fade-down"  data-aos-duration="700" ><p id="quote">"Let them say I walked with giants"</p>
+
+    <a href="point.html"><div id="view">VIEW</div></a></div>
+    
+    </button></div>`
+     
+        
+   
+
+}
+
+function FAQ3() {
+
+
+    test3.innerHTML = `<div id="test3"><button id="accordion" onclick="closing3()">
+    
+    <div id="big"><p id="call">Toss And Turn</p></div>
+    
+    <div id="small"><ion-icon name="remove-outline"></ion-icon></div>
+    
+    <div id="move-el" data-aos="fade-down"  data-aos-duration="700" ><p id="quote">"Cruel and brutal life we chained to, the living are in pieces others picked to cease to exist, for the silence."</p>
+
+    <a href="toss and turn.html"><div id="view">VIEW</div></a></div>
+    
+    </button></div>`
+     
+        
+   
+
+}
+
+function FAQ4() {
+
+
+    test4.innerHTML = `<div id="test4"><button id="accordion" onclick="closing4()">
+    
+    <div id="big"><p id="call">Keep Staring</p></div>
+    
+    <div id="small"><ion-icon name="remove-outline"></ion-icon></div>
+    
+    <div id="move-el" data-aos="fade-down" data-aos-duration="700" > <p id="quote">"Thousand reasons you won't make it, lots are better than you.
+     The competition with thousand dazzling, but no one is you"</p>
+
+     <a href="keepStaring.html"><div id="view">VIEW</div></a></div>
+    
+    </button></div>`
+     
+        
+   
+
+}
+
+
+function FAQ5() {
+
+
+    test5.innerHTML = `<div id="test5"><button id="accordion" onclick="closing5()">
+    
+    <div id="big"><p id="call">Love Song</p></div>
+    
+    <div id="small"><ion-icon name="remove-outline"></ion-icon></div>
+    
+    <div id="move-el" data-aos="fade-down" data-aos-duration="1000"><div id="move-el" data-aos="fade-down" ><p id="quote">"What would really hurt, is you not knowing who I am.
+     So what if this game requires me to compromise who I really am? "</p>
+
+     <a href="loveSong.html"><div id="view">VIEW</div></a></div></div>
+    
+    </button></div>`
+     
+        
+   
+
+}
+
+
+function FAQ6() {
+
+
+    test6.innerHTML = `<div id="test6"><button id="accordion" onclick="closing6()">
+    
+    <div id="big"><p id="call">That's Me</p></div>
+    
+    <div id="small"><ion-icon name="remove-outline"></ion-icon></div>
+    
+    <div id="move-el" data-aos="fade-down"  data-aos-duration="700"><p id="quote">"As much as i'm yearning not to make it, i'm willing
+    to follow my destiny, spend some time with my innerself, regardless of how I should be doing things, and that's me.
+    "</p> 
+
+    <a href="that'sMe.html"><div id="view">VIEW</div></a></div>
+    
+    </button></div>`
+     
+        
+   
+
+}
+
+
+
+
+function closing() {
+
+
+    test.innerHTML = `<div id="test"><button id="accordion" onclick="FAQ()">
+
+        <div id="big"><p id="call">Win</p></div>
+
+        <div id="small"><ion-icon name="add-outline"></ion-icon></div>
+
+    </button></div>`
+
+    
+
+
+    
+}
+
+
+function closing2() {
+
+
+    test2.innerHTML = `<div id="test2"><button id="accordion" onclick="FAQ2()">
+
+        <div id="big"><p id="call">At This Point</p></div>
+
+        <div id="small"><ion-icon name="add-outline"></ion-icon></div>
+
+    </button></div>`
+
+    
+
+
+    
+}
+
+
+
+
+
+
+function closing3() {
+
+
+    test3.innerHTML = `<div id="test3"><button id="accordion" onclick="FAQ3()">
+
+        <div id="big"><p id="call">Toss And Turn</p></div>
+
+        <div id="small"><ion-icon name="add-outline"></ion-icon></div>
+
+    </button></div>`
+
+    
+
+   
+}
+
+
+
+function closing4() {
+
+
+    test4.innerHTML = `<div id="test3"><button id="accordion" onclick="FAQ4()">
+
+        <div id="big"><p id="call">Keep Staring</p></div>
+
+        <div id="small"><ion-icon name="add-outline"></ion-icon></div>
+
+    </button></div>`
+
+    
+    
+}
+
+
+function closing5() {
+
+
+    test5.innerHTML = `<div id="test3"><button id="accordion" onclick="FAQ5()">
+
+        <div id="big"><p id="call">Love Song</p></div>
+
+        <div id="small"><ion-icon name="add-outline"></ion-icon></div>
+
+    </button></div>`
+
+    
+    
+}
+
+
+function closing6() {
+
+
+    test6.innerHTML = `<div id="test3"><button id="accordion" onclick="FAQ6()">
+
+        <div id="big"><p id="call">That's Me</p></div>
+
+        <div id="small"><ion-icon name="add-outline"></ion-icon></div>
+
+    </button></div>`
+
+    
+    
+}
+
+
+
+
+
+
+
+// ----------------------------ABOUT WIN------------------------------- //
+
+
+let wrote = document.getElementById('support')
+let side = document.getElementById('side')
+
+function winDescription() {
+
+
+    wrote.innerHTML = ` <div id="support">
+
+        
+    <p id="siding">01 WIN</p>
+    <div id="side"><p id="topping">ABOUT WIN</p>
+
+    <p id="written">"Win” is the opening track in Live's debut mixtape That's Me.
+    On this song Live talks about his dreams of filling a spot in the hip hop Industry,
+    and how he wishes that it embraces him. <br><br>
+    While big artists worry about staying relevant, Live is searching for relevance.
+    This song not only reveals his intent to make a name for himself,
+    but also reveals his frustrations towards his father, for reasons which are further explored in the mixtape.
+    <br>
+    <br>
+    
+    We uncover that Live's issues with his father affect his relationship with his little brother;
+    and that he yearns to not only inspire him but to also grow closer to the rest of his estranged family. <br><br> 
+    This song also reveals a sense of urgency in Live's life,
+    we learn that he is under pressure to be financially independent. <br><br>
+    He explicitly states that he feels like a burden to his grandmother, 
+    regardless of how she continues to believe and invest in his dreams.
+    And while under this immense pressure to be successful, 
+    Live has some self doubts, which he takes us through on the following song “At this point”. <br>
+    <a href="win.html"><span style="color:#a63b00"> Read Less</span></a><br>
+    
+    </p>
+
+    <a href="https://linktr.ee/iam_live?fbclid=PAAaYjMGtNPPJTmqR6Ek65HP09gk45vYk8phr2bRUghFp1bWWzygD-gd3ZPpA" target="_blank"><div id="listening"><button>LISTEN</button></div></a>
+
+    <div id="rhyme">
+
+        <img src="images/cover.jpg" alt="Live">
+
+      
+<ul>
+            <li><p id="write"> <b>Win</b></p></li>
+            <li><p id="artist">Live Mxaku</p></li>
+        </ul>
+
+
+        <p id="written"> <span style="color: #003b40;">I told my mom that I'm gonna reach my prime <br>
+            I'm corny mom coz that's everybody's life <br>
+            Some made it and some failed it,
+            I don't know why</span> <br>
+            <span style="color:#a63b00"> Read More</span>
+        </p>
+    
+    </div>
+
+    <a href="lyrics.html"><div id="mic"><button>LYRICS</button></div></a>
+
+</div>
+
+
+
+</div>
+
+
+
+    <style>
+    
+    @media only screen and (min-width: 200px) {
+        #side{
+            overflow: hidden;
+                margin: 0;
+                width: 100%;
+                border-style: none
+               
+                
+               
+            }
+
+            #support{
+                overflow: hidden;
+               
+            }
+
+
+           
+
+            #listening{
+                margin: auto;
+                
+            }
+
+            
+
+            @media only screen and (max-width: 500px) {
+                #siding{
+                    display:none;
+                     
+                   
+                 }
+            }
+
+
+            @media only screen and (min-width: 500px) {
+
+                #support{
+                    position: relative;
+                   
+                   
+                  line-height: normal;
+                 
+                 width: 85%;
+                margin: auto;
+                border-style: none;
+               
+        
+               
+               
+                
+                }
+
+                #siding{
+                    position: fixed;
+                    z-index: -1;
+                    
+                  
+                }
+
+
+                #side{
+                    width:85%;
+                        border-style: solid;
+                        border-color: black;
+                        border-width: thin;
+                        margin-left: 10%;
+                       
+                      
+                        
+                       
+                    }
+
+                    
+
+
+                    @media only screen and (min-width: 1000px) {
+
+
+                        #support{
+                            position: relative;
+                           
+                           
+                          line-height: normal;
+                         
+                         width: 80%;
+                        margin: auto;
+                        
+                        border-style: none;
+                       
+                        
+                        }
+
+
+                        #siding{
+                            position: absolute;
+                            margin-right: 5%
+                          
+                        }
+
+                        #side{
+                    width:60%;
+                            border-style: solid;
+                            border-width: thin;
+                            border-color: black;
+                            margin-left: 20%
+                           
+                        }
+
+
+                    }
+
+            }
+
+        }
+
+          
+
+
+            </style>
+    
+    `
+  
+}
+
+
+
+
+
+//--------------------------------------------------LYRICS---------------------------------------------//
+
+
+function winLyrics() {
+
+
+    wrote.innerHTML = `<div id="support">
+
+      
+
+  
+
+    <div id="rhyme">
+
+        <div id="holder"><img src="images/cover.jpg" alt="Live">
+
+      
+<ul>
+            <li><p id="write"> <b>Win</b></p></li>
+            <li><p id="artist">Live Mxaku</p></li>
+        </ul></div>
+
+
+        <p id="written"> <span style="color: #003b40;"> [Verse]<br>
+        I told my mom that I'm gonna reach my prime<br>
+        I'm corny mom coz that's everybody's life<br>
+        Some made it and some failed it
+        I don't know why<br>
+        Pick a goal you wanna score
+        the questions how?<br>
+        Never give up is old school but also true<br>
+        Some day you gonna pray you make it too<br>
+        Die like a giant and the day you die they shook<br>
+        Street wet of tears, man they loved you too<br>
+        Just planning my life man surviving too<br>
+        Tell my haters to keep hating it's all they can do<br>
+        Say it's luck I'm fortunate when l can't lose<br>
+        If l die today my name dies today<br>
+        Now I'm working my ass off to make it live<br>
+        I'm not special I'm just joining the game to win<br>
+        And see what l can offer for the game 'til when<br>
+        23 years old and still dependent?<br>
+        This is too much pressure especially grandma<br>
+        Picture feeding your grandson you not even sure<br>
+        Where you wasting your time when you was feeding this boy?<br>
+        I know you believe in me grandma you don't have choice<br>
+        Lil' brother l know we not close because <br>
+        With our family issue we never gonna be close<br>
+        Pray it's never too late, promise I'll be back<br>
+        I wanna come back with something and see the smile on your face<br>
+        Everything your dad says is wrong 'bout me<br>
+        And it's hard to tell who my family is<br>
+        They say a man is no man with family<br>
+        God save me I'm losing my manhood again<br>
+        I've studied the game l saw the man losing his fans,<br>
+        These are your fans you can't call 'em family<br>
+        You lost your fans? You gonna lose friends<br>
+        Replace the friend you always thought you had<br>
+        Nowadays I'm mad because it's hard to find<br>
+        A fan to listen to this song l made<br>
+        I'm gonna force people to respect my name<br>
+        And when l move the mountain do not say it's luck<br>
+        To rap like this it never takes a day<br>
+        It don't matter whether I'm really working hard or not<br>
+        The plan is just to keep working or die<br> 
+        I do shit somehow you will be say class<br>
+        The next shit, you gonna be saying it's trash<br>
+        Who cares as long as l don't hate myself<br> 
+        Big artists are worrying about the state of the game<br>
+        They wanna know if they still connect with fans<br>
+        I wonder if I'm gonna connect at all<br>
+        I got a lot to say though I got no time<br> 
+        For punchlines and word plays cause that will change<br>
+        The narrative of what l wanna say<br>
+        Good fellas this is not to entertain yourselves<br> 
+        I'm tryna catch your ear, you might be interested by<br>
+        Shit l wanna say to you l write my<br>
+        Lines without having to compromise them<br>
+        They plain and that's a lack of creating punchlines<br> 
+        Who cares about your top lists goats and shit,<br>
+        This poor land of mine you think l care<br>
+        About people saying I'm good at what l do?<br>
+        I do care about that but now I got a mission<br>
+        To get rich and spend time with my family<br> 
+        But now my circle is becoming smaller<br>
+        And to be honest l think I'm scared as fuck(shit)<br>
+        To be alone in this alarming world.</span> <br>
+            <span style="color:#a63b00"> Read Less</span>
+        </p>
+
+        <a href="lyrics.html"><div id="mic"><button>LYRICS</button></div></a>
+
+        <div id="side"><p id="topping">ABOUT WIN</p>
+
+        <p id="write">"Win” is the opening track in Live's debut mixtape "That's Me".
+            On this song Live talks about his dreams of filling a spot in the hip hop Industry,
+            and how he wishes that it embraces him. <br>
+           <span style="color:#a63b00" onclick="winDescription()"> Read More</span>
+        </p>
+    
+       </div>
+
+
+       <a href="https://linktr.ee/iam_live?fbclid=PAAaYjMGtNPPJTmqR6Ek65HP09gk45vYk8phr2bRUghFp1bWWzygD-gd3ZPpA" target="_blank"><div id="listening"><button>LISTEN</button></div></a>
+    
+    </div>
+
+
+ 
+
+
+
+
+</div>
+
+
+
+<style>
+
+@media only screen and (min-width: 200px) {
+
+    #rhyme{
+        margin-top:0;
+        
+        width:100%;
+        border-style: none;
+        
+    }
+
+    #mic button{
+        margin: auto;
+        margin: 5%
+        
+    }
+
+    #written{
+        padding-top: 45%;
+    }
+
+
+    #holder{
+        position:fixed;
+        background-color: #f3fbfb;
+        z-index: 1;
+    }
+
+
+    #side{
+        position: relative;
+        background-color: #fff;
+       
+        overflow: hidden;
+            margin: auto;
+            width: 90%;
+            border:solid;
+            border-width: thin;
+           padding:0
+           
+           
+           
+        }
+
+
+       
+
+        #support{
+         
+            overflow: hidden;
+           
+        }
+
+
+       
+
+        #listening button{
+            width:90%;
+            margin: auto;
+            margin: 5%
+            
+        }
+
+    
+
+   
+}
+
+</style>
+
+
+
+
+`
+
+
+    
+}
+
+
+
+
+
+
+
